91ecf00a3150cfbfe99bffcc8621b37b568c5a61
nidhijagga <nidhijagga5239@gmail.com>
2023-08-03 17:16:17 +0530

    Footer Organised

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 /**
@@ -26,9 +27,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 
-const Footer = () => {
-  return <h4>dfn kjbdf kjfg</h4>;
-};
 
 const AppLayout = () => {
   return (
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <h4>dfn kjbdf kjfg</h4>;
+  };
+
+export default Footer;
