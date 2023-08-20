f92afe64b37cd15abd179408de3c54041b3f2fae
nidhijagga <nidhijagga5239@gmail.com>
2023-08-20 17:05:49 +0530

    Search Test Case - NC

---
+++ src/components/__tests__/search.test.js
@@ -0,0 +1,24 @@
+// import { render } from "@testing-library/react";
+// import Body from "../Body";
+// import { Provider } from "react-redux";
+// import store from "../../utils/store";
+// import { StaticRouter } from "react-router-dom/server";
+
+// global.fetch = jest.fn(()=>{
+//     return Promise.resolve({
+//         json : Promise.resolve()
+//     })
+// });
+
+// test("Search Results", () => {
+//   const body = render(
+//     <StaticRouter>
+//       <Provider store={store}>
+//         <Body/>
+//       </Provider>
+//     </StaticRouter>
+//   );
+// });
+
+
+//Not Completed
